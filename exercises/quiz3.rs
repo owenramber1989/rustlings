10f20152df5ab9dc20957aa79bed190a2886622b
owenramber1989 <owenramber1989@163.com>
2023-09-12 15:10:24 +0800

    <checkpoint>: <finished part 3>

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,15 +14,15 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt::{Display, Debug};
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T: Display> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display + Debug> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -50,13 +50,13 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
+            grade: "A+".to_string(),
+            student_name: "Jinming Zhang".to_string(),
             student_age: 11,
         };
         assert_eq!(
             report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
+            "Jinming Zhang (11) - achieved a grade of A+"
         );
     }
 }
